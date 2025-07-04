f8295248a05c56e14ecd850df5064f6cc5dac18e
Anita Chouhan <fe.dev2.ba2024@gmail.com>
2025-07-04 19:18:35 +0530

    updated domain

--- components/cv/coverletterform/IntroductionAndBodyForm.jsx
+++ components/cv/coverletterform/IntroductionAndBodyForm.jsx
@@ -43,7 +43,7 @@ const IntroductionAndBodyForm = () => {
 
     if (index === 0) {
       endpoint =
-        "https://api.createmyresume.in/api/user/aisummery-section1-coverletter";
+        "https://rbgenapi.ultraaura.education/api/user/aisummery-section1-coverletter";
       payload = {
         name: personalDetails.name,
         target_role: personalDetails.position,
@@ -52,13 +52,13 @@ const IntroductionAndBodyForm = () => {
       };
     } else if (index === 1) {
       endpoint =
-        "https://api.createmyresume.in/api/user/aisummery-section2-coverletter";
+        "https://rbgenapi.ultraaura.education/api/user/aisummery-section2-coverletter";
       payload = {
         target_role: personalDetails.position,
       };
     } else if (index === 2) {
       endpoint =
-        "https://api.createmyresume.in/api/user/aisummery-section3-coverletter";
+        "https://rbgenapi.ultraaura.education/api/user/aisummery-section3-coverletter";
       payload = {
         name: personalDetails.name,
         target_role: personalDetails.position,
